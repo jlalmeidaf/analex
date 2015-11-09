6e2ab4d4ecc91f3189313c5b4f14b07835d5a920
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2015-11-09 18:59:32 -0200

    Added codigo.txt and insert code for added word in list2

---
+++ compilador_python/table_tokens.py
@@ -0,0 +1,20 @@
+import re 
+def create_lst(): #Cria uma lista com todas as palavras do documento
+
+	lst = []
+	lst2 = []
+	#name = raw_input("Enter the document name: ")
+	with open("codigo.txt", "r") as f: #Abre o documento
+		rows = f.readlines()	#Armazena todas as linhas, em forma de lista, do documento em rows
+	f.close()
+	for i in xrange(len(rows)): 
+		result = re.search("^[//].*$", rows[i]) #Expressão regular para retirada dos comentários
+		if result:
+			pass
+		else:
+			lst.append(rows[i].split()) #Coloca na lista dividindo entre os espaços
+	for row in lst:
+		for word in row:
+			lst2.append(word)
+	
+	return lst2
