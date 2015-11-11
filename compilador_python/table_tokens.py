d5a8d27ef87c4c0d272f529bfdeb51c3a78d5093
Livia Freitas <freitas.li@hotmail.com>
2015-11-11 00:10:10 -0200

    retirei codigo desnecessario

--- compilador_python/table_tokens.py
+++ compilador_python/table_tokens.py
@@ -5,9 +5,7 @@ def create_lst(): #Cria uma lista com todas as palavras do documento
 	lst 	= []
 	lst2 	= []
 	var 	= []
-	delimi 	= []
-	typeInt = []
-	aux		= []
+	
 	#name = raw_input("Enter the document name: ")
 	with open("codigo.txt", "r") as f: #Abre o documento
 		rows = f.readlines()	#Armazena todas as linhas, em forma de lista, do documento em rows
@@ -44,26 +42,24 @@ def create_lst(): #Cria uma lista com todas as palavras do documento
 def table_token(lst_words,var): #Cria a tabela de tokens
 
 	PR = ["var","program","integer","end;","begin","goto",'loop']
-	NUM = ['0','1','2','3','4','5','6','7','8','9','1;']
-	ID = ['x','L1','helloword','y']
+	NUM = ['0','1','2','3','4','5','6','7','8','9','1;', '0;']
+	ID = ['x','L1','helloword','y', 'L1;']
 	# criar identificadores com regex
 	CE = [",","(",")"]
-	Operador = ['<-','+']
+	OP = ['<-','+']
 	
 	with open("table.txt", "w") as table: #Cria tabela um novo documento
 		for words in lst_words:
 			if words in PR:
 				table.write("%s:Palavra Reservada \n" % words)
 				print("%s:Palavra Reservada \n" % words) 
-			#elif words in ID:
-			#	table.write("%s:Identificador \n" % words)
 			elif words in CE:
 				table.write("%s:Caracter Especial\n" % words)
 				print("%s:Palavra Reservada \n" % words)
 			elif words in ID:
-				table.write("%s:IDENTIFICADOR\n" % words)
-				print("%s:IDENTIFICADOR\n" % words)
-			elif words in Operador:
+				table.write("%s:Identificador\n" % words)
+				print("%s:Identificador\n" % words)
+			elif words in OP:
 				table.write("%s:Operador\n" % words)
 				print("%s:Operador\n" % words)
 			elif words in NUM:
